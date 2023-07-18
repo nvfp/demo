7160d82716a3ab6e7ad652866097056d087dfe9a
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-17 22:42:31 -0700

    retest: tweak script to improve readability

--- test-dir/test-py.py
+++ test-dir/test-py.py
@@ -14,10 +14,10 @@ print(f'arg2: {repr(args.arg2)}')
 pth1 = os.path.join(args.arg1, 'bar', 'baz')
 pth2 = os.path.join(args.arg2, 'bar', 'baz')
 
-print('---')
+print('====')
 print(f'pth1: {pth1}')
 print(f'pth2: {pth2}')
 
-print('---')
+print('====')
 print(f'repr(pth1): {repr(pth1)}')
 print(f'repr(pth2): {repr(pth2)}')
