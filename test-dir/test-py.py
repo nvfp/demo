9fc8947149e63d425c93c2bb7de3b47a95b3180e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-17 22:40:43 -0700

    Test concatenating Linux path via Bash shell in Python script

--- test-dir/test-py.py
+++ test-dir/test-py.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 
 parser = argparse.ArgumentParser()
@@ -9,3 +10,14 @@ args = parser.parse_args()
 
 print(f'arg1: {repr(args.arg1)}')
 print(f'arg2: {repr(args.arg2)}')
+
+pth1 = os.path.join(args.arg1, 'bar', 'baz')
+pth2 = os.path.join(args.arg2, 'bar', 'baz')
+
+print('---')
+print(f'pth1: {pth1}')
+print(f'pth2: {pth2}')
+
+print('---')
+print(f'repr(pth1): {repr(pth1)}')
+print(f'repr(pth2): {repr(pth2)}')
