6ed1ba8729a416e67c3268b918f2f07b6d3993f6
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-26 05:00:06 -0700

    test making file and checking it in another branch

--- test-dir/test.py
+++ test-dir/test.py
@@ -2,3 +2,6 @@ import os
 
 pth = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(f'pth: {repr(pth)}')
+
+with open(os.path.join(pth, 'foobarbaz321.txt'), 'w') as f:
+    f.write('hello mom!')
