b7afbc28ebbfe5b22faf7c084408b7bd402de010
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-26 05:08:42 -0700

    the file is gone when switching branches using actions/checkout; let's test using a temp folder to carry the file!

--- test-dir/test.py
+++ test-dir/test.py
@@ -1,7 +1,11 @@
 import os
 
-pth = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+pth = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 print(f'pth: {repr(pth)}')
 
-with open(os.path.join(pth, 'foobarbaz321.txt'), 'w') as f:
+dir = os.path.join(pth, 'arch_temp')
+print(dir)
+
+os.mkdir(dir)
+with open(os.path.join(dir, 'foobarbaz321.txt'), 'w') as f:
     f.write('hello mom!')
