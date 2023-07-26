35f256326724b93b012c7dd551a986d80260e22c
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-26 05:18:09 -0700

    test committing the file in another branch

--- test-dir/test.py
+++ test-dir/test.py
@@ -4,7 +4,7 @@ pth = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))
 print(f'pth: {repr(pth)}')
 
 dir = os.path.join(pth, 'arch_temp')
-print(dir)
+print(f'dir: {repr(dir)}')
 
 os.mkdir(dir)
 with open(os.path.join(dir, 'foobarbaz321.txt'), 'w') as f:
